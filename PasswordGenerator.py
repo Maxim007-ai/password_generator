0088ac8b10f1d7c19ee2131327dd3c3b38aadd9a
Phantomkai18 <rden4556018@gmail.com>
2026-07-18 21:33:33 +0300

    Разработал класс PasswordGenerator

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,6 +1,6 @@
 import random
 import string
-
+import tkinter as tk
 class PasswordGenerator:
     def __init__(self):
         self.lowercase_chars = string.ascii_lowercase
@@ -15,3 +15,23 @@ class PasswordGenerator:
         digits_part = [random.choice(self.digits_chars) for _ in range(num_digits)]
         special_part = [random.choice(self.special_chars) for _ in range(num_special)]
         return ''.join(lower_part + upper_part + digits_part + special_part)
+
+class PasswordGeneratorGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Генератор паролей")
+        self.gen = PasswordGenerator()
+        # простейшая кнопка
+        btn = tk.Button(root, text="Сгенерировать", command=self.on_generate)
+        btn.pack()
+        self.result = tk.Label(root, text="")
+        self.result.pack()
+
+    def on_generate(self):
+        pwd = self.gen.generate(3,2,2,2)
+        self.result.config(text=pwd)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PasswordGeneratorGUI(root)
+    root.mainloop()
